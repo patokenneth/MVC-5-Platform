cf86276ee097dab6098e853a819dd6f5f4307f10
patokenneth <chiemezieoloto4@gmail.com>
2019-08-09 13:43:01 +0100

    firstCommit

---
+++ SimpleApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using SimpleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(Color color)
+        {
+            Color? oldColor = Session["color"] as Color?;
+
+            if (oldColor != null)
+            {
+                Votes.ChangeVote(color, (Color)oldColor);
+            }
+            else
+            {
+                Votes.RecordVote(color);
+            }
+
+            ViewBag.SelectedColor = Session["color"] = color;
+            return View();
+        }
+    }
+}
